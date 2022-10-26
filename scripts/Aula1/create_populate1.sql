f6827146702755b150220187e982c07859c41e09
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2022-10-26 10:17:04 -0300

    Formated Aual1 scripts

--- scripts/Aula1/create_populate1.sql
+++ scripts/Aula1/create_populate1.sql
@@ -1,284 +1,374 @@
- -- Criando tabela de aluno
-create table
-  aluno (
-    id number not null constraint aluno_pk primary key,
-    primeiro_nome varchar2 (15) not null,
-    ultimo_nome varchar2 (15) not null,
-    endereco varchar2 (100) not null
-  );
+-- Criando tabela de aluno
+create table ALUNO (
+  ID NUMBER not null constraint ALUNO_PK primary key,
+  PRIMEIRO_NOME varchar2 (15) not null,
+  ULTIMO_NOME varchar2 (15) not null,
+  ENDERECO varchar2 (100) not null
+);
 
 -- Criando tabela de curso
-create table
-  curso (
-    id number not null constraint curso_pk primary key,
-    nome varchar2 (30) not null
-  );
+create table CURSO (
+  ID NUMBER not null constraint CURSO_PK primary key,
+  NOME varchar2 (30) not null
+);
 
 -- Criando conexão entre tabela de aluno e curso
-create table
-  con_aluno_curso (
-    aluno_id number not null,
-    curso_id number not null
-  );
+create table CON_ALUNO_CURSO (
+  ALUNO_ID NUMBER not null,
+  CURSO_ID NUMBER not null
+);
 
 -- Criando tabela de mensalidade
-create table
-  mensalidade (
-    id number not null constraint mensalidade_pk primary key,
-    aluno_id number not null,
-    data_vencimento date not null,
-    data_emissao date not null,
-    valor number not null,
-    descricao varchar2 (200)
-  );
+create table MENSALIDADE (
+  ID NUMBER not null constraint MENSALIDADE_PK primary key,
+  ALUNO_ID NUMBER not null,
+  DATA_VENCIMENTO DATE not null,
+  DATA_EMISSAO DATE not null,
+  VALOR NUMBER not null,
+  DESCRICAO varchar2 (200)
+);
 
 -- Criando tabela de pagamento
-create table
-  pagamento (
-    id number not null constraint pagamento_pk primary key,
-    data_pagamento date not null,
-    valor_pago number not null
-  );
+create table PAGAMENTO (
+  ID NUMBER not null constraint PAGAMENTO_PK primary key,
+  DATA_PAGAMENTO DATE not null,
+  VALOR_PAGO NUMBER not null
+);
 
 -- Criando tabela para fazer relção vários para vários entre mensalidade e pagamento
-create table
-  con_mensalidade_pagamento (
-    mensalidade_id number not null,
-    pagamento_id number not null
-  );
+create table CON_MENSALIDADE_PAGAMENTO (
+  MENSALIDADE_ID NUMBER not null,
+  PAGAMENTO_ID NUMBER not null
+);
 
 -- Inserindo aluno
-insert into
-  aluno (id, primeiro_nome, ultimo_nome, endereco)
-values
-  (1, 'Guilherme', 'Casagrande', 'Rua topzera 1');
+insert into ALUNO (
+  ID,
+  PRIMEIRO_NOME,
+  ULTIMO_NOME,
+  ENDERECO
+) values (
+  1,
+  'Guilherme',
+  'Casagrande',
+  'Rua topzera 1'
+);
 
-insert into
-  aluno (id, primeiro_nome, ultimo_nome, endereco)
-values
-  (2, 'Flávia', 'Jonathan', 'Rua maneirassa 2');
+insert into ALUNO (
+  ID,
+  PRIMEIRO_NOME,
+  ULTIMO_NOME,
+  ENDERECO
+) values (
+  2,
+  'Flávia',
+  'Jonathan',
+  'Rua maneirassa 2'
+);
 
-insert into
-  aluno (id, primeiro_nome, ultimo_nome, endereco)
-values
-  (3, 'Felipe', 'Jonathan', 'Rua maneirassa 3');
+insert into ALUNO (
+  ID,
+  PRIMEIRO_NOME,
+  ULTIMO_NOME,
+  ENDERECO
+) values (
+  3,
+  'Felipe',
+  'Jonathan',
+  'Rua maneirassa 3'
+);
 
 -- Inserindo cursos
-insert into
-  curso (id, nome)
-values
-  (1, 'Curso legal 1');
+insert into CURSO (
+  ID,
+  NOME
+) values (
+  1,
+  'Curso legal 1'
+);
 
-insert into
-  curso (id, nome)
-values
-  (2, 'Curso legal 2');
+insert into CURSO (
+  ID,
+  NOME
+) values (
+  2,
+  'Curso legal 2'
+);
 
 -- Conectando alunos aos cursos
-insert into
-  con_aluno_curso (aluno_id, curso_id)
-values
-  (1, 1);
+insert into CON_ALUNO_CURSO (
+  ALUNO_ID,
+  CURSO_ID
+) values (
+  1,
+  1
+);
 
-insert into
-  con_aluno_curso (aluno_id, curso_id)
-values
-  (2, 1);
+insert into CON_ALUNO_CURSO (
+  ALUNO_ID,
+  CURSO_ID
+) values (
+  2,
+  1
+);
 
-insert into
-  con_aluno_curso (aluno_id, curso_id)
-values
-  (2, 2);
+insert into CON_ALUNO_CURSO (
+  ALUNO_ID,
+  CURSO_ID
+) values (
+  2,
+  2
+);
 
-insert into
-  con_aluno_curso (aluno_id, curso_id)
-values
-  (3, 2);
+insert into CON_ALUNO_CURSO (
+  ALUNO_ID,
+  CURSO_ID
+) values (
+  3,
+  2
+);
 
 -- Inserindo mensalidade
-insert into
-  mensalidade (
-    id,
-    aluno_id,
-    data_vencimento,
-    data_emissao,
-    valor,
-    descricao
-  )
-values
-  (
-    1,
-    1,
-    '22-SEP-22',
-    '14-SEP-22',
-    4512.22,
-    'Mensalidade de setembro'
-  );
+insert into MENSALIDADE (
+  ID,
+  ALUNO_ID,
+  DATA_VENCIMENTO,
+  DATA_EMISSAO,
+  VALOR,
+  DESCRICAO
+) values (
+  1,
+  1,
+  '22-SEP-22',
+  '14-SEP-22',
+  4512.22,
+  'Mensalidade de setembro'
+);
 
-insert into
-  mensalidade (
-    id,
-    aluno_id,
-    data_vencimento,
-    data_emissao,
-    valor,
-    descricao
-  )
-values
-  (
-    2,
-    1,
-    '22-OCT-22',
-    '14-OCT-22',
-    200.00,
-    'Mensalidade de outubro'
-  );
+insert into MENSALIDADE (
+  ID,
+  ALUNO_ID,
+  DATA_VENCIMENTO,
+  DATA_EMISSAO,
+  VALOR,
+  DESCRICAO
+) values (
+  2,
+  1,
+  '22-OCT-22',
+  '14-OCT-22',
+  200.00,
+  'Mensalidade de outubro'
+);
 
-insert into
-  mensalidade (
-    id,
-    aluno_id,
-    data_vencimento,
-    data_emissao,
-    valor,
-    descricao
-  )
-values
-  (
-    3,
-    2,
-    '22-SEP-22',
-    '14-SEP-22',
-    4512.22,
-    'Mensalidade de setembro'
-  );
+insert into MENSALIDADE (
+  ID,
+  ALUNO_ID,
+  DATA_VENCIMENTO,
+  DATA_EMISSAO,
+  VALOR,
+  DESCRICAO
+) values (
+  3,
+  2,
+  '22-SEP-22',
+  '14-SEP-22',
+  4512.22,
+  'Mensalidade de setembro'
+);
 
-insert into
-  mensalidade (
-    id,
-    aluno_id,
-    data_vencimento,
-    data_emissao,
-    valor,
-    descricao
-  )
-values
-  (
-    4,
-    2,
-    '22-OCT-22',
-    '14-OCT-22',
-    200.00,
-    'Mensalidade de outubro'
-  );
+insert into MENSALIDADE (
+  ID,
+  ALUNO_ID,
+  DATA_VENCIMENTO,
+  DATA_EMISSAO,
+  VALOR,
+  DESCRICAO
+) values (
+  4,
+  2,
+  '22-OCT-22',
+  '14-OCT-22',
+  200.00,
+  'Mensalidade de outubro'
+);
 
-insert into
-  mensalidade (
-    id,
-    aluno_id,
-    data_vencimento,
-    data_emissao,
-    valor,
-    descricao
-  )
-values
-  (
-    5,
-    3,
-    '22-AUG-22',
-    '14-AUG-22',
-    200.00,
-    'Mensalidade de agosto'
-  );
+insert into MENSALIDADE (
+  ID,
+  ALUNO_ID,
+  DATA_VENCIMENTO,
+  DATA_EMISSAO,
+  VALOR,
+  DESCRICAO
+) values (
+  5,
+  3,
+  '22-AUG-22',
+  '14-AUG-22',
+  200.00,
+  'Mensalidade de agosto'
+);
 
 -- Inserindo pagamentos
-insert into
-  pagamento (id, data_pagamento, valor_pago)
-values
-  (1, '18-SEP-22', 2500.22);
+insert into PAGAMENTO (
+  ID,
+  DATA_PAGAMENTO,
+  VALOR_PAGO
+) values (
+  1,
+  '18-SEP-22',
+  2500.22
+);
 
-insert into
-  pagamento (id, data_pagamento, valor_pago)
-values
-  (2, '19-SEP-22', 12.00);
+insert into PAGAMENTO (
+  ID,
+  DATA_PAGAMENTO,
+  VALOR_PAGO
+) values (
+  2,
+  '19-SEP-22',
+  12.00
+);
 
-insert into
-  pagamento (id, data_pagamento, valor_pago)
-values
-  (3, '20-SEP-22', 200.00);
+insert into PAGAMENTO (
+  ID,
+  DATA_PAGAMENTO,
+  VALOR_PAGO
+) values (
+  3,
+  '20-SEP-22',
+  200.00
+);
 
-insert into
-  pagamento (id, data_pagamento, valor_pago)
-values
-  (4, '21-OCT-22', 200.00);
+insert into PAGAMENTO (
+  ID,
+  DATA_PAGAMENTO,
+  VALOR_PAGO
+) values (
+  4,
+  '21-OCT-22',
+  200.00
+);
 
-insert into
-  pagamento (id, data_pagamento, valor_pago)
-values
-  (5, '18-SEP-22', 2500.22);
+insert into PAGAMENTO (
+  ID,
+  DATA_PAGAMENTO,
+  VALOR_PAGO
+) values (
+  5,
+  '18-SEP-22',
+  2500.22
+);
 
-insert into
-  pagamento (id, data_pagamento, valor_pago)
-values
-  (6, '19-SEP-22', 12.00);
+insert into PAGAMENTO (
+  ID,
+  DATA_PAGAMENTO,
+  VALOR_PAGO
+) values (
+  6,
+  '19-SEP-22',
+  12.00
+);
 
-insert into
-  pagamento (id, data_pagamento, valor_pago)
-values
-  (7, '20-SEP-22', 200.00);
+insert into PAGAMENTO (
+  ID,
+  DATA_PAGAMENTO,
+  VALOR_PAGO
+) values (
+  7,
+  '20-SEP-22',
+  200.00
+);
 
-insert into
-  pagamento (id, data_pagamento, valor_pago)
-values
-  (8, '21-OCT-22', 200.00);
+insert into PAGAMENTO (
+  ID,
+  DATA_PAGAMENTO,
+  VALOR_PAGO
+) values (
+  8,
+  '21-OCT-22',
+  200.00
+);
 
-insert into
-  pagamento (id, data_pagamento, valor_pago)
-values
-  (9, '21-AUG-22', 200.00);
+insert into PAGAMENTO (
+  ID,
+  DATA_PAGAMENTO,
+  VALOR_PAGO
+) values (
+  9,
+  '21-AUG-22',
+  200.00
+);
 
 -- Inserindo a conexão entre mensalidade e pagamentos
-insert into
-  con_mensalidade_pagamento (mensalidade_id, pagamento_id)
-values
-  (1, 1);
+insert into CON_MENSALIDADE_PAGAMENTO (
+  MENSALIDADE_ID,
+  PAGAMENTO_ID
+) values (
+  1,
+  1
+);
 
-insert into
-  con_mensalidade_pagamento (mensalidade_id, pagamento_id)
-values
-  (1, 2);
+insert into CON_MENSALIDADE_PAGAMENTO (
+  MENSALIDADE_ID,
+  PAGAMENTO_ID
+) values (
+  1,
+  2
+);
 
-insert into
-  con_mensalidade_pagamento (mensalidade_id, pagamento_id)
-values
-  (1, 3);
+insert into CON_MENSALIDADE_PAGAMENTO (
+  MENSALIDADE_ID,
+  PAGAMENTO_ID
+) values (
+  1,
+  3
+);
 
-insert into
-  con_mensalidade_pagamento (mensalidade_id, pagamento_id)
-values
-  (2, 4);
+insert into CON_MENSALIDADE_PAGAMENTO (
+  MENSALIDADE_ID,
+  PAGAMENTO_ID
+) values (
+  2,
+  4
+);
 
-insert into
-  con_mensalidade_pagamento (mensalidade_id, pagamento_id)
-values
-  (3, 5);
+insert into CON_MENSALIDADE_PAGAMENTO (
+  MENSALIDADE_ID,
+  PAGAMENTO_ID
+) values (
+  3,
+  5
+);
 
-insert into
-  con_mensalidade_pagamento (mensalidade_id, pagamento_id)
-values
-  (3, 6);
+insert into CON_MENSALIDADE_PAGAMENTO (
+  MENSALIDADE_ID,
+  PAGAMENTO_ID
+) values (
+  3,
+  6
+);
 
-insert into
-  con_mensalidade_pagamento (mensalidade_id, pagamento_id)
-values
-  (3, 7);
+insert into CON_MENSALIDADE_PAGAMENTO (
+  MENSALIDADE_ID,
+  PAGAMENTO_ID
+) values (
+  3,
+  7
+);
 
-insert into
-  con_mensalidade_pagamento (mensalidade_id, pagamento_id)
-values
-  (4, 8);
+insert into CON_MENSALIDADE_PAGAMENTO (
+  MENSALIDADE_ID,
+  PAGAMENTO_ID
+) values (
+  4,
+  8
+);
 
-insert into
-  con_mensalidade_pagamento (mensalidade_id, pagamento_id)
-values
-  (5, 9);
+insert into CON_MENSALIDADE_PAGAMENTO (
+  MENSALIDADE_ID,
+  PAGAMENTO_ID
+) values (
+  5,
+  9
+);
--- scripts/Aula1/pl_sql1.sql
+++ scripts/Aula1/pl_sql1.sql
@@ -1,90 +1,100 @@
--- Questão 2: Criar uma view que exiba o nome, o endereço, a data de vencimento e o 
--- valor de todos os alunos que não efetuaram pagamento até a presente 
+-- Questão 2: Criar uma view que exiba o nome, o endereço, a data de vencimento e o
+-- valor de todos os alunos que não efetuaram pagamento até a presente
 -- data.
-create view
-  alunos_faltando_pagar_ate_hoje as
-select
-  (a.primeiro_nome || ' ' || a.ultimo_nome) as nome_aluno,
-  a.endereco as endereco_aluno,
-  m.data_vencimento as vencimento_boleto,
-  m.valor as valor_boleto,
-  sum(p.valor_pago) as total_pago
-from
-  aluno a
-  inner join mensalidade m on a.id = m.aluno_id
-  inner join con_mensalidade_pagamento cmp on cmp.mensalidade_id = m.id
-  inner join pagamento p on p.id = cmp.pagamento_id
-group by
-  a.id,
-  a.primeiro_nome,
-  a.ultimo_nome,
-  a.endereco,
-  m.id,
-  m.data_vencimento,
-  m.valor,
-  cmp.mensalidade_id
-having
-  sum(p.valor_pago) != m.valor;
+create view ALUNOS_FALTANDO_PAGAR_ATE_HOJE as
+  select
+    (A.PRIMEIRO_NOME || ' ' || A.ULTIMO_NOME) as NOME_ALUNO,
+    A.ENDERECO                                as ENDERECO_ALUNO,
+    M.DATA_VENCIMENTO                         as VENCIMENTO_BOLETO,
+    M.VALOR                                   as VALOR_BOLETO,
+    sum (P.VALOR_PAGO)                         as TOTAL_PAGO
+  from
+    ALUNO                     A
+    inner join MENSALIDADE M
+    on A.ID = M.ALUNO_ID
+    inner join CON_MENSALIDADE_PAGAMENTO CMP
+    on CMP.MENSALIDADE_ID = M.ID
+    inner join PAGAMENTO P
+    on P.ID = CMP.PAGAMENTO_ID
+  group by
+    A.ID,
+    A.PRIMEIRO_NOME,
+    A.ULTIMO_NOME,
+    A.ENDERECO,
+    M.ID,
+    M.DATA_VENCIMENTO,
+    M.VALOR,
+    CMP.MENSALIDADE_ID
+  having
+    sum (P.VALOR_PAGO) != M.VALOR;
 
--- Questão 3: Criar uma view que exiba a matricula e o nome dos alunos que não 
+-- Questão 3: Criar uma view que exiba a matricula e o nome dos alunos que não
 -- tenham mensalidades emitidas.
-create view
-  alunos_que_nao_emitiram_mensalidade as
-select
-  a.id as matricula_aluno,
-  a.primeiro_nome
-from
-  aluno a
-  left join mensalidade m on m.aluno_id = a.id
-group by
-  a.id,
-  a.primeiro_nome,
-  to_char (sysdate, 'MM')
-having
-  to_char (sysdate, 'MM') > max(to_char (m.data_emissao, 'MM'));
+create view ALUNOS_QUE_NAO_EMITIRAM_MENSALIDADE as
+  select
+    A.ID as MATRICULA_ALUNO,
+    A.PRIMEIRO_NOME
+  from
+    ALUNO       A
+    left join MENSALIDADE M
+    on M.ALUNO_ID = A.ID
+  group by
+    A.ID,
+    A.PRIMEIRO_NOME,
+    TO_CHAR (SYSDATE,
+    'MM')
+  having
+    TO_CHAR (SYSDATE,
+    'MM') > max (TO_CHAR (M.DATA_EMISSAO,
+    'MM') );
 
--- Questão 4: Criar uma view que exiba o nome do aluno, o nome do curso e a 
--- quantidade de dias entre a data de vencimento e data atual de alunos que 
+-- Questão 4: Criar uma view que exiba o nome do aluno, o nome do curso e a
+-- quantidade de dias entre a data de vencimento e data atual de alunos que
 -- pagam a mensalidade com antecedência
-create view
-  alunos_intervalo_antecedencia_pagamento_mensalidade as
-select
-  (a.primeiro_nome || ' ' || a.ultimo_nome) as nome_aluno,
-  cur.nome as nome_curso,
-  (sysdate - max(p.data_pagamento)) * -1 as diferenca_dias
-from
-  aluno a
-  inner join con_aluno_curso cac on cac.aluno_id = a.id
-  inner join curso cur on cur.id = cac.curso_id
-  inner join mensalidade m on m.aluno_id = a.id
-  inner join con_mensalidade_pagamento cmp on cmp.mensalidade_id = m.id
-  inner join pagamento p on p.id = cmp.pagamento_id
-group by
-  (a.primeiro_nome || ' ' || a.ultimo_nome),
-  cur.nome,
-  m.valor,
-  m.data_vencimento
-having
-  m.valor = sum(p.valor_pago)
-  and m.data_vencimento > max(p.data_pagamento)
-  and (sysdate - max(p.data_pagamento)) * -1 >= 0;
+create view ALUNOS_INTERVALO_ANTECEDENCIA_PAGAMENTO_MENSALIDADE as
+  select
+    (A.PRIMEIRO_NOME || ' ' || A.ULTIMO_NOME) as NOME_ALUNO,
+    CUR.NOME                                  as NOME_CURSO,
+    (SYSDATE - max (P.DATA_PAGAMENTO) ) * - 1    as DIFERENCA_DIAS
+  from
+    ALUNO                     A
+    inner join CON_ALUNO_CURSO CAC
+    on CAC.ALUNO_ID = A.ID
+    inner join CURSO CUR
+    on CUR.ID = CAC.CURSO_ID
+    inner join MENSALIDADE M
+    on M.ALUNO_ID = A.ID
+    inner join CON_MENSALIDADE_PAGAMENTO CMP
+    on CMP.MENSALIDADE_ID = M.ID
+    inner join PAGAMENTO P
+    on P.ID = CMP.PAGAMENTO_ID
+  group by
+    (A.PRIMEIRO_NOME
+      || ' '
+      || A.ULTIMO_NOME),
+    CUR.NOME,
+    M.VALOR,
+    M.DATA_VENCIMENTO
+  having
+    M.VALOR = sum (P.VALOR_PAGO)
+    and M.DATA_VENCIMENTO > max (P.DATA_PAGAMENTO)
+    and (SYSDATE - max (P.DATA_PAGAMENTO) ) * - 1 >= 0;
 
 -- Questão 5: Consultar estas views usando o comando SELECT
 select
   *
 from
-  alunos_faltando_pagar_ate_hoje;
+  ALUNOS_FALTANDO_PAGAR_ATE_HOJE;
 
 select
   *
 from
-  alunos_que_nao_emitiram_mensalidade;
+  ALUNOS_QUE_NAO_EMITIRAM_MENSALIDADE;
 
 select
   *
 from
-  alunos_intervalo_antecedencia_pagamento_mensalidade;
+  ALUNOS_INTERVALO_ANTECEDENCIA_PAGAMENTO_MENSALIDADE;
 
 -- Questão 6: Apagar a view criada no exercício de número 2
-drop
-  view alunos_faltando_pagar_ate_hoje;
+drop view ALUNOS_FALTANDO_PAGAR_ATE_HOJE;
